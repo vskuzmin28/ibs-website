6407489869f7b34f3d07b9f205c9f9087d735afd
vskuzmin28 <vskuzmin28@gmail.com>
2023-09-19 21:34:05 +0300

    added slider for component projects

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -19,3 +19,85 @@ $(document).scroll(function() {
       $('.header').removeClass('header--float')
     }
 })
+
+// sliders
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  let mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    autoHeight: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.controls__next',
+      prevEl: '.controls__prev',
+    },
+    
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      576: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 2,
+      },
+
+    },
+    on: {
+      init: function() {
+        checkArrow();
+      },
+      resize: function () {
+        checkArrow();
+      }
+    },
+  })
+});
+
+// function checkArrow() {
+//   var swiperPrev = document.querySelector('.swiper-button-prev');
+//   var swiperNext = document.querySelector('.swiper-button-next');
+//   if ( window.innerWidth > 450 ) {
+//     //console.log('Success', window.innerWidth);
+//     swiperPrev.style.display = 'block';
+//     swiperNext.style.display = 'block';
+//   } else {
+//     swiperPrev.style.display = 'none';
+//     swiperNext.style.display = 'none';
+//   }
+// }
+
+$('.projects__slider').each(function(){
+  let swiper = new Swiper('.projects__slider', {
+      // Optional parameters
+      direction: 'horizontal',
+      slidesPerView: 1,
+      spaceBetween: 30,
+      
+      noSwipingClass: "projects__slider",
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          },
+          navigation: {
+            nextEl: '.controls__next',
+            prevEl: '.controls__prev',
+          },
+          //effect: 'fade',
+  })
+});
